1d6ba5855be1b694a7441b942d3f9e210b901797
goutham <gowthamphani07@gmail.com>
2026-06-24 15:17:51 +0530

    Initial Commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Jenkins");
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println(
+            "Sum of " + a + " and " + b + " is " + (a+b)
+        );
+    }
+}
